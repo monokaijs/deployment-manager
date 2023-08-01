d3d92b8f3a80360263430d15be99d1db392d1239
Edward Nguyen <monokaijs@gmail.com>
2023-08-01 19:01:24 +0700

    :sparkles: add prerequisite packages

---
+++ src/types/app.d.ts
@@ -0,0 +1,19 @@
+interface Server {
+  name: string;
+  connectionType: "ssh";
+  address: string;
+  username: string;
+  password: string;
+  privateKey: string;
+}
+
+interface App {
+  name: string;
+  dockerImage: string;
+  variables: string[];
+}
+
+interface AppEnvironment {
+  name: string;
+  variables: string[];
+}
